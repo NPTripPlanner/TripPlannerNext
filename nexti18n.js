3cfdc7a983cf6fedf37c29d136a2c9853be460ce
Nelson <tomneo2004@gmail.com>
2020-10-03 12:46:19 +0800

    move localization file to locales

--- nexti18n.js
+++ nexti18n.js
@@ -8,5 +8,5 @@ module.exports = new NextI18Next({
   otherLanguages: ['zht'],
   defaultLanguage: 'en',
   // localeSubpaths,
-  localePath: path.resolve('./public/static/locales')
+  localePath: path.resolve('./locales')
 })
